70b597ebd91eb6d15bfb189790aabe4ade777777
Jubaer Ahamed <jubaerahamed@gmail.com>
2021-08-04 02:24:16 +0600

    Design Menu Item

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,14 +1,23 @@
+import { size } from 'lodash';
 import React from 'react'
+import {Card, CardImg, CardImgOverlay, CardBody, CardTitle} from 'reactstrap'
 
 const MenuItem = props =>{
     console.log(props);
     return(
         <div>
+            <Card style={{margin:"10px auto"}}>
+                <CardBody >
+                    <CardImg width="100%" alt={props.dish.name} src={props.dish.image} style={{opacity:"0.7"}}/>
+                    <CardImgOverlay>
+                        <CardTitle style={{textAlign:"center", fontSize:"30px", fontWeight:"bold",margin:"auto"}}>{props.dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </CardBody>
+
+            </Card>
             
         </div>
     )
 }
-
-
 
 export default MenuItem;
